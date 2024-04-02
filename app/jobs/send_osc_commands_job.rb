f2cc6c86fdacbd128df5eea15aa5e73ab6d6ccfc
Avram Walden <aviemet@gmail.com>
2024-04-02 15:39:09 -0700

    fix: fixes error handling on osc job

--- app/jobs/send_osc_commands_job.rb
+++ app/jobs/send_osc_commands_job.rb
@@ -5,13 +5,15 @@ class SendOscCommandsJob < ApplicationJob
     server_commands = protocol.commands.group_by(&:server)
 
     server_commands.each do |server, commands|
+      ap({ server:, commands: })
       client = OscService::Client.new(host: server.hostname, port: server.port)
 
       commands.each do |command|
+        ap({ message: OscService::Message.new(command.message) })
         client.send(OscService::Message.new(command.message))
       end
     end
   rescue Errno::ECONNREFUSED
-    server.create_activity key: :hostname
+    protocol.commands.first.server.create_activity key: :hostname
   end
 end
