db0bc6a04b9c6a95585e22ac04eaf7016d490d5c
Avram Walden <aviemet@gmail.com>
2024-04-02 16:46:47 -0700

    feat: casts message value before sending OSC command

--- app/jobs/send_osc_commands_job.rb
+++ app/jobs/send_osc_commands_job.rb
@@ -2,18 +2,38 @@ class SendOscCommandsJob < ApplicationJob
   queue_as :default
 
   def perform(protocol)
-    server_commands = protocol.commands.group_by(&:server)
+    server_commands = protocol.commands.select('commands.*, protocols_commands.*').group_by(&:server)
 
     server_commands.each do |server, commands|
       ap({ server:, commands: })
       client = OscService::Client.new(host: server.hostname, port: server.port)
 
       commands.each do |command|
-        ap({ message: OscService::Message.new(command.message) })
-        client.send(OscService::Message.new(command.message))
+        ap({ message: OscService::Message.new(command.message, cast(command.value, command.payload_type)) })
+        client.send(OscService::Message.new(command.message, cast(command.value, command.payload_type)))
       end
     end
-  rescue Errno::ECONNREFUSED
+  rescue Errno::ECONNREFUSED => e
+    ap({ connection: e })
     protocol.commands.first.server.create_activity key: :hostname
+  rescue StandardError => e
+    ap({ other: e })
+  end
+
+  def cast(value, type)
+    case type
+    when "integer"
+      value.to_i
+    when "float"
+      value.to_f
+    when "blob"
+      value.bytes
+    when "time"
+      Date.parse(value)
+    when "boolean"
+      ActiveModel::Type::Boolean.new.cast(value)
+    else
+      value
+    end
   end
 end
