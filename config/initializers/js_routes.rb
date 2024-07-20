45c5522291ff0aebee60ef5f1929c0eaacdcfe20
Avram Walden <aviemet@gmail.com>
2024-07-20 09:56:32 -0700

    fix: moves again

--- config/initializers/js_routes.rb
+++ config/initializers/js_routes.rb
@@ -5,5 +5,5 @@ JsRoutes.setup do |config|
   config.exclude = [/^rails/, /^new_rails/]
   config.compact = true
   config.camel_case = true
-  config.file = Rails.root.join("app/frontend/lib/routes/index.ts")
+  config.file = Rails.root.join("app/frontend/lib/routes/routes.ts")
 end
