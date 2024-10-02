93bf4d1bfc237d1b53248f00d4a27174cd08bbf6
Avram Walden <aviemet@gmail.com>
2024-10-02 08:53:21 -0700

    feat: spec annotations

--- spec/models/remote_api_spec.rb
+++ spec/models/remote_api_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: remote_apis
+#
+#  id          :bigint           not null, primary key
+#  auth_token  :string
+#  description :text
+#  root_url    :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe RemoteApi, type: :model do
--- spec/models/remote_endpoint_spec.rb
+++ spec/models/remote_endpoint_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: remote_endpoints
+#
+#  id            :bigint           not null, primary key
+#  endpoint      :string
+#  title         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  remote_api_id :bigint           not null
+#
+# Indexes
+#
+#  index_remote_endpoints_on_remote_api_id  (remote_api_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (remote_api_id => remote_apis.id)
+#
 require 'rails_helper'
 
 RSpec.describe RemoteEndpoint, type: :model do
